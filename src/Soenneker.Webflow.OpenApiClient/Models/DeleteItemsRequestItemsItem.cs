05b77ad3aff4cc2d335ad2874609e7921e130ad5
Jake Soenneker <jake@soenneker.com>
2026-08-20 17:24:23 +0000

    Automated update

--- src/Soenneker.Webflow.OpenApiClient/Models/DeleteItemsRequestItemsItem.cs
+++ src/Soenneker.Webflow.OpenApiClient/Models/DeleteItemsRequestItemsItem.cs
@@ -14,7 +14,7 @@ namespace Soenneker.Webflow.OpenApiClient.Models
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>Array of identifiers for the locales where the item will be created</summary>
+        /// <summary>Array of identifiers for the locales where the item will be deleted</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
         public List<string>? CmsLocaleIds { get; set; }
